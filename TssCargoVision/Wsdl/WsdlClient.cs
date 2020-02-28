9ad86375b602a6075ea5243ff753d0f30a86dc8e
NET Guy <m.sadegh.sh@gmail.com>
2020-02-28 16:30:13 +0330

    Abandon ASMX

--- TssCargoVision/Wsdl/WsdlClient.cs
+++ TssCargoVision/Wsdl/WsdlClient.cs
@@ -43,45 +43,6 @@ namespace TssCargoVision.Wsdl
             return await rawResponse.Content.ReadAsAsync<TResponse>();
         }
 
-        //private TResponse UnwrapEnvelope<TResponse>(string input)
-        //{
-        //    var document = new XmlDocument();
-        //    document.LoadXml(input);
-
-        //    XmlNamespaceManager nsmgr = new XmlNamespaceManager(document.NameTable);
-        //    nsmgr.AddNamespace("SOAP-ENV", "http://schemas.xmlsoap.org/soap/envelope/");
-        //    nsmgr.AddNamespace("ns1", "http://tss.ir/cargoVision/service/index");
-
-        //    return _xmlStreamSerializer.Deserialize<TResponse>(
-        //        document.SelectSingleNode($"SOAP-ENV:Envelope/SOAP-ENV:Body/ns1:{typeof(TResponse).Name}", nsmgr).InnerXml
-        //    );
-        //}
-
-        //private string WrapEnvelope<TBody>(TBody input)
-        //{
-        //    var action = typeof(TBody).Name.Replace("Request", string.Empty);
-        //    var properties = input
-        //        .GetType()
-        //        .GetProperties(BindingFlags.Instance | BindingFlags.Public)
-        //        .OfType<PropertyInfo>();
-
-        //    var nameValues = properties
-        //        .Select(p => $"<{p.Name}>{p.GetValue(input)}</{p.Name}>");
-
-        //    var flattenedValues = string.Join(Environment.NewLine, nameValues);
-
-        //    return $@"<?xml version=""1.0"" encoding=""UTF-8""?>
-        //    <SOAP-ENV:Envelope xmlns:SOAP-ENV=""http://schemas.xmlsoap.org/soap/envelope/"" xmlns:ns1=""http://tss.ir/cargoVision/service/index"">
-        //      <SOAP-ENV:Body>
-        //        <ns1:{action}>
-        //          <input>
-        //            {flattenedValues}
-        //          </input>
-        //        </ns1:{action}>
-        //      </SOAP-ENV:Body>
-        //    </SOAP-ENV:Envelope>";
-        //}
-
         public void Dispose()
         {
             _httpClient?.Dispose();
